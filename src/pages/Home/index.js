49261e3d4673cc35a3d4ee547320a17a03f95f7f
Giulia Kirinus <giulia.kirinus@gmail.com>
2022-09-26 12:02:19 -0300

    6

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,26 @@
 import React from "react";
 import Navbar from "./navbar";
+import '../Home/style.css'
 
 const HomePage = () => {
 
 
     return(
-     <div>
+
+      <div>
+
         <div>
           <Navbar/>
+          </div>
+        <div className="corpo">
+        <div>
+          <h3>A melhor forma de armazenar as informações da sua farmacia é aqui no Pharmacy Manegement!</h3>
+        </div>
         </div>
 
-       
+        </div> 
 
-     </div>
+    
     )
 }
 
