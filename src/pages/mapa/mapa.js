895e1ffd810b3d989de9bc2977eceaa03727d84c
Giulia Kirinus <giulia.kirinus@gmail.com>
2022-09-25 23:23:14 -0300

    commit 3

--- src/pages/mapa/mapa.js
+++ src/pages/mapa/mapa.js
@@ -1,29 +1,20 @@
-
-import {MapContainer,TileLayer,Marker,Popup} from 'react-leaflet'
-
+import React from 'react'
+import { MapContainer, TileLayer} from 'react-leaflet'
 import '../mapa/mapa.css'
 
-const Mapa = () => {
-   
-    return(
-        <div>
-            <h1>Consulte o mapa de Farmacias</h1>
-            <p>Econtre a farmacia mais próxima a sua</p> 
 
-            <div className='mapa'>
-            <MapContainer center={[-32.0332, -52.0986]} zoom={13} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
-            </MapContainer>
-            </div>
-        </div>
-    )
+const Mapa = () => {
+
+        return(
+            <MapContainer center={[-32.0332, -52.0986]} zoom={12}scrollWheelZoom={false}>
+            <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            />
+          </MapContainer>
+
+ )
 }
+
+
 export default Mapa;
